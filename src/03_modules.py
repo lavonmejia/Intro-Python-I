a1d798862c4356b30fd469bb55578942f594c3ec
LaVon Mejia <lavonmejia@gmail.com>
2019-12-03 19:18:54 -0500

    sys complete still need import os

--- src/03_modules.py
+++ src/03_modules.py
@@ -10,12 +10,17 @@ import sys
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
+# sys.argv <--learning return type
+for arg in sys.argv:
+    print(arg)
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
+print(sys.platform)
 
 # Print out the version of Python you're using:
 # YOUR CODE HERE
+print(sys.version_info)
 
 
 import os
